8206f5fc8e4f01333366b87101255ffc9fc0d970
Upendra Chauhan <upendra100279@gmail.com>
2025-10-04 13:16:50 +0530

    Added some links

--- Project/backend/src/app.js
+++ Project/backend/src/app.js
@@ -8,7 +8,7 @@ const cors=require('cors')
 const app=express()
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://mern-stack-project-heg6.vercel.app/",
     credentials:true
 }))
 app.use(cookieParser());
